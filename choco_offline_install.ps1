1d4e9cc00b68787fd4527eb88a12fb843fe0a721
RalfEs73 <ralf@essbaumer.de>
2021-01-13 13:24:03 +0100

    Update choco_offline_install.ps1

--- choco_offline_install.ps1
+++ choco_offline_install.ps1
@@ -3,12 +3,12 @@ $CurrentUserName = $CurrentUser.split("\")[1]
 
 Write-Host "Installing Chocolatey"
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-	choco install chocolatey-core.extension -y
-	Write-Host "Done"
+choco install chocolatey-core.extension -y
+choco feature enable --name=allowGlobalConfirmation
+Write-Host "Done"
 
 Write-Host "Installing Chocolatey GUI"
 choco install ChocolateyGUI -y
-choco feature enable --name=allowGlobalConfirmation
 Write-Host "Done"
 
 

4a1922033bce8c0c276c59f34308d001aa8fd86e
RalfEs73 <ralf@essbaumer.de>
2021-01-22 20:43:28 +0100

    Add Windows Terminal

--- README.md
+++ README.md
@@ -40,6 +40,7 @@ iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercon
 * Edge (Chromium)
 * WinRAR
 * gsudo
+* Windows Terminal
 * GeForce Experience
 * WinSCP
 * Notepad++
--- win10_reinstall.ps1
+++ win10_reinstall.ps1
@@ -27,6 +27,12 @@ Write-Host "Installing gsudo"
 choco install gsudo
 Write-Host "Done"
 
+Write-Host "Installing Windows Termial"
+choco install microsoft-windows-terminal
+mkdir "C:\Users\$CurrentUserName\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
+Start-BitsTransfer -Source "https://github.com/RalfEs73/win10_reinstall/raw/master/Config%20Files/Windows%20Terminal/settings.json" -Destination "C:\Users\$CurrentUserName\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
+Write-Host "Done"
+
 Write-Host "Installing GeForce Experience"
 choco install geforce-experience -y
 Write-Host "Done"
@@ -142,12 +148,6 @@ Start-BitsTransfer -Source "https://raw.githubusercontent.com/RalfEs73/chocolate
 Start-BitsTransfer -Source "https://raw.githubusercontent.com/RalfEs73/chocolatey-packages/master/filminfo/settings/Genre.xml" -Destination "C:\Users\$CurrentUserName\AppData\Roaming\film info! organizer\Genre.xml"
 Start-BitsTransfer -Source "https://raw.githubusercontent.com/RalfEs73/chocolatey-packages/master/filminfo/settings/Interface.xml" -Destination "C:\Users\$CurrentUserName\AppData\Roaming\film info! organizer\Interface.xml"
 Start-BitsTransfer -Source "https://raw.githubusercontent.com/RalfEs73/chocolatey-packages/master/filminfo/settings/tmdb_config.xml" -Destination "C:\Users\$CurrentUserName\AppData\Roaming\film info! organizer\tmdb_config.xml"
-Write-Host "Done"
-
-Write-Host "Installiere Config-Files"
-# Windows Terminal
-mkdir "C:\Users\$CurrentUserName\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
-Start-BitsTransfer -Source "https://github.com/RalfEs73/win10_reinstall/raw/master/Config%20Files/Windows%20Terminal/settings.json" -Destination "C:\Users\$CurrentUserName\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
 Write-Host "Done"
 
 Write-Host "Deinstalliere Bloatware"

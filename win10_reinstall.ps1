b98de8c7016d04b40b84a004e6f75e34714b26b9
RalfEs73 <ralf@essbaumer.de>
2021-01-30 20:09:00 +0100

    Update win10_reinstall.ps1

--- win10_reinstall.ps1
+++ win10_reinstall.ps1
@@ -1,135 +1,138 @@
 # iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/RalfEs73/win10_reinstall/master/win10_reinstall.ps1'))
 
+New-Item -Path "C:\Temp\ChocoCache" -ItemType directory -Force | Out-Null
+$cache = "--cacheLocation=C:\Temp\ChocoCache"
+
 $CurrentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
 $CurrentUserName = $CurrentUser.split("\")[1]
 
 Write-Host "Installing Chocolatey"
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco install chocolatey-core.extension -y
+choco install chocolatey-core.extension -y $cache
 choco feature enable --name=allowGlobalConfirmation
 Write-Host "Done"
 
 Write-Host "Installing Chocolatey GUI"
-choco install ChocolateyGUI -y
+choco install ChocolateyGUI -y $cache
 Write-Host "Done"
 
 Write-Host "Installing Edge (Chromium)"
-choco install microsoft-edge -y
+choco install microsoft-edge -y $cache
 del "C:\Users\Public\Desktop\Microsoft Edge.lnk"
 Write-Host "Done"
 
 Write-Host "Installing WinRAR"
-choco install winrar -y
+choco install winrar -y $cache
 del "C:\Users\$CurrentUserName\Desktop\PeaZip.lnk"
 Write-Host "Done"
 
 Write-Host "Installing gsudo"
-choco install gsudo
+choco install gsudo -y $cache
 Write-Host "Done"
 
 Write-Host "Installing Windows Termial"
 choco install microsoft-windows-terminal
-mkdir "C:\Users\$CurrentUserName\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
+New-Item -Path "C:\Users\$CurrentUserName\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState" -ItemType directory -Force | Out-Null
 Start-BitsTransfer -Source "https://github.com/RalfEs73/win10_reinstall/raw/master/Config%20Files/Windows%20Terminal/settings.json" -Destination "C:\Users\$CurrentUserName\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
 Write-Host "Done"
 
 Write-Host "Installing GeForce Experience"
-choco install geforce-experience -y
+choco install geforce-experience -y $cache
 Write-Host "Done"
 
 Write-Host "Installing ModernFlyouts"
-choco install modernflyouts -y
+choco install modernflyouts -y $cache
 Write-Host "Done"
 
 Write-Host "Installing WinSCP"
-choco install winscp.install -y
+choco install winscp.install -y $cache
 del "C:\Users\Public\Desktop\WinSCP.lnk"
 Write-Host "Done"
 
 Write-Host "Installing Notepad++"
-choco install notepadplusplus.install -y
+choco install notepadplusplus.install -y $cache
 Write-Host "Done"
 
 Write-Host "Installing 4K Video Downloader"
-choco install 4k-video-downloader -y
+choco install 4k-video-downloader -y $cache
 del "C:\Users\Public\Desktop\4K Video Downloader.lnk"
 Write-Host "Done"
 
 Write-Host "Installing Image Resizer for Windows"
-choco install imageresizerapp -y
+choco install imageresizerapp -y $cache
 Write-Host "Done"
 
 Write-Host "Installing VLC"
-choco install vlc -y
+choco install vlc -y $cache
 del "C:\Users\Public\Desktop\VLC media player.lnk"
 Write-Host "Done"
 
 Write-Host "Installing Plex"
-choco install plex -y
+choco install plex -y $cache
 Write-Host "Done"
 
 Write-Host "Installing AnyDVD"
-choco install anydvd -y
+choco install anydvd -y $cache
 del "C:\Users\Public\Desktop\AnyDVD.lnk"
 Write-Host "Done"
 
 Write-Host "Installing File Converter"
-choco install file-converter -y
+choco install file-converter -y $cache
 Write-Host "Done"
 
 Write-Host "Installing HandBrake"
-choco install handbrake.install -y
+choco install handbrake.install -y $cache
 del "C:\Users\Public\Desktop\Handbrake.lnk"
 Write-Host "Done"
 
 Write-Host "Installing GitHub Desktop"
-choco install github-desktop -y
+choco install github-desktop -y $cache
 del "C:\Users\$CurrentUserName\Desktop\GitHub Desktop.lnk"
 Stop-Process -Name GitHubDesktop
 Write-Host "Done"
 
 Write-Host "Installing Telegram"
-choco install telegram.install -y
+choco install telegram.install -y $cache
 del "C:\Users\$CurrentUserName\Desktop\Telegram.lnk"
 Write-Host "Done"
 
 Write-Host "Installing WhatsApp"
-choco install WhatsApp -y
+choco install WhatsApp -y $cache
 del "C:\Users\$CurrentUserName\Desktop\WhatsApp.lnk"
 Write-Host "Done"
 
 Write-Host "Installing Discord"
-choco install discord.install -y
+choco install discord.install -y $cache
 del "C:\Users\$CurrentUserName\Desktop\Discord.lnk"
 Write-Host "Done"
 
 Write-Host "Installing Epic Games Launcher"
-choco install epicgameslauncher -y
+choco install epicgameslauncher -y $cache
 del "C:\Users\Public\Desktop\Epic Games Launcher.lnk"
 Write-Host "Done"
 
 Write-Host "Installing Steam"
-choco install steam -y
+choco install steam -y $cache
 del "C:\Users\Public\Desktop\Steam.lnk"
 Write-Host "Done"
 
 Write-Host "Installing Uplay"
-choco install uplay -y --ignore-checksums
+choco install uplay -y $cache --ignore-checksums
 del "C:\Users\$CurrentUserName\Desktop\Ubisoft Connect.lnk"
 Write-Host "Done"
 
 Write-Host "Installing Streamlabs OBS"
-choco install streamlabs-obs -y
+choco install streamlabs-obs -y $cache
 del "C:\Users\Public\Desktop\Streamlabs OBS.lnk"
 Write-Host "Done"
 
 Write-Host "Installing Spotify"
-choco install spotify -y --ignore-checksums
+choco install spotify -y $cache --ignore-checksums
 del "C:\Users\$CurrentUserName\Desktop\Spotify.lnk"
 Write-Host "Done"
 
 Write-Host "Installing Driver Booster"
-choco install driverbooster -y --ignore-checksums
+choco install driverbooster -y $cache --ignore-checksums
 del "C:\Users\Public\Desktop\Driver Booster 8.lnk"
 Stop-Process -Name Driver*
 Write-Host "Done"
@@ -144,7 +147,7 @@ Write-Host "Installing Film Info! Organiser"
 # Not listed yet
 choco install filminfo --version=0.6.1.3
 del "C:\Users\Public\Desktop\Film Info! Organizer.lnk"
-mkdir "C:\Users\$CurrentUserName\AppData\Roaming\film info! organizer"
+New-Item -Path "C:\Users\$CurrentUserName\AppData\Roaming\film info! organizer" -ItemType directory -Force | Out-Null
 Start-BitsTransfer -Source "https://raw.githubusercontent.com/RalfEs73/chocolatey-packages/master/filminfo/settings/Columns.xml" -Destination "C:\Users\$CurrentUserName\AppData\Roaming\film info! organizer\Columns.xml"
 Start-BitsTransfer -Source "https://raw.githubusercontent.com/RalfEs73/chocolatey-packages/master/filminfo/settings/Config.xml" -Destination "C:\Users\$CurrentUserName\AppData\Roaming\film info! organizer\Config.xml"
 Start-BitsTransfer -Source "https://raw.githubusercontent.com/RalfEs73/chocolatey-packages/master/filminfo/settings/Favoriten.xml" -Destination "C:\Users\$CurrentUserName\AppData\Roaming\film info! organizer\Favoriten.xml"

b437752bd57f93bb1b8bd58ec42f165b1416800d
RalfEs73 <ralf@essbaumer.de>
2021-01-31 22:38:25 +0100

    Update win10_reinstall.ps1

--- win10_reinstall.ps1
+++ win10_reinstall.ps1
@@ -22,12 +22,11 @@ Write-Host "Done"
 
 Write-Host "Installing Edge (Chromium)"
 choco install microsoft-edge -y $cache
-del "C:\Users\Public\Desktop\Microsoft Edge.lnk"
+Remove-Item "C:\Users\Public\Desktop\Microsoft Edge.lnk"
 Write-Host "Done"
 
 Write-Host "Installing WinRAR"
 choco install winrar -y $cache
-del "C:\Users\$CurrentUserName\Desktop\PeaZip.lnk"
 Write-Host "Done"
 
 Write-Host "Installing gsudo"
@@ -50,7 +49,7 @@ Write-Host "Done"
 
 Write-Host "Installing WinSCP"
 choco install winscp.install -y $cache
-del "C:\Users\Public\Desktop\WinSCP.lnk"
+Remove-Item "C:\Users\Public\Desktop\WinSCP.lnk"
 Write-Host "Done"
 
 Write-Host "Installing Notepad++"
@@ -59,7 +58,7 @@ Write-Host "Done"
 
 Write-Host "Installing 4K Video Downloader"
 choco install 4k-video-downloader -y $cache
-del "C:\Users\Public\Desktop\4K Video Downloader.lnk"
+Remove-Item "C:\Users\Public\Desktop\4K Video Downloader.lnk"
 Write-Host "Done"
 
 Write-Host "Installing Image Resizer for Windows"
@@ -68,7 +67,7 @@ Write-Host "Done"
 
 Write-Host "Installing VLC"
 choco install vlc -y $cache
-del "C:\Users\Public\Desktop\VLC media player.lnk"
+Remove-Item "C:\Users\Public\Desktop\VLC media player.lnk"
 Write-Host "Done"
 
 Write-Host "Installing Plex"
@@ -77,12 +76,12 @@ Write-Host "Done"
 
 Write-Host "Installing AnyDVD"
 choco install anydvd -y $cache
-del "C:\Users\Public\Desktop\AnyDVD.lnk"
+Remove-Item "C:\Users\Public\Desktop\AnyDVD.lnk"
 Write-Host "Done"
 
 Write-Host "Installing AnyStream"
 choco install anystream --version=1.0.7.0 -y $cache
-del "C:\Users\Public\Desktop\AnyStream.lnk"
+Remove-Item "C:\Users\Public\Desktop\AnyStream.lnk"
 Write-Host "Done"
 
 Write-Host "Installing File Converter"
@@ -91,71 +90,71 @@ Write-Host "Done"
 
 Write-Host "Installing HandBrake"
 choco install handbrake.install -y $cache
-del "C:\Users\Public\Desktop\Handbrake.lnk"
+Remove-Item "C:\Users\Public\Desktop\Handbrake.lnk"
 Write-Host "Done"
 
 Write-Host "Installing GitHub Desktop"
 choco install github-desktop -y $cache
-del "C:\Users\$CurrentUserName\Desktop\GitHub Desktop.lnk"
+Remove-Item "C:\Users\$CurrentUserName\Desktop\GitHub Desktop.lnk"
 Stop-Process -Name GitHubDesktop
 Write-Host "Done"
 
 Write-Host "Installing Telegram"
 choco install telegram.install -y $cache
-del "C:\Users\$CurrentUserName\Desktop\Telegram.lnk"
+Remove-Item "C:\Users\$CurrentUserName\Desktop\Telegram.lnk"
 Write-Host "Done"
 
 Write-Host "Installing WhatsApp"
 choco install WhatsApp -y $cache
-del "C:\Users\$CurrentUserName\Desktop\WhatsApp.lnk"
+Remove-Item "C:\Users\$CurrentUserName\Desktop\WhatsApp.lnk"
 Write-Host "Done"
 
 Write-Host "Installing Discord"
 choco install discord.install -y $cache
-del "C:\Users\$CurrentUserName\Desktop\Discord.lnk"
+Remove-Item "C:\Users\$CurrentUserName\Desktop\Discord.lnk"
 Write-Host "Done"
 
 Write-Host "Installing Epic Games Launcher"
 choco install epicgameslauncher -y $cache
-del "C:\Users\Public\Desktop\Epic Games Launcher.lnk"
+Remove-Item "C:\Users\Public\Desktop\Epic Games Launcher.lnk"
 Write-Host "Done"
 
 Write-Host "Installing Steam"
 choco install steam -y $cache
-del "C:\Users\Public\Desktop\Steam.lnk"
+Remove-Item "C:\Users\Public\Desktop\Steam.lnk"
 Write-Host "Done"
 
 Write-Host "Installing Uplay"
 choco install uplay -y $cache --ignore-checksums
-del "C:\Users\$CurrentUserName\Desktop\Ubisoft Connect.lnk"
+Remove-Item "C:\Users\$CurrentUserName\Desktop\Ubisoft Connect.lnk"
 Write-Host "Done"
 
 Write-Host "Installing Streamlabs OBS"
 choco install streamlabs-obs -y $cache
-del "C:\Users\Public\Desktop\Streamlabs OBS.lnk"
+Remove-Item "C:\Users\Public\Desktop\Streamlabs OBS.lnk"
 Write-Host "Done"
 
 Write-Host "Installing Spotify"
 choco install spotify -y $cache --ignore-checksums
-del "C:\Users\$CurrentUserName\Desktop\Spotify.lnk"
+Remove-Item "C:\Users\$CurrentUserName\Desktop\Spotify.lnk"
 Write-Host "Done"
 
 Write-Host "Installing Driver Booster"
 choco install driverbooster -y $cache --ignore-checksums
-del "C:\Users\Public\Desktop\Driver Booster 8.lnk"
+Remove-Item "C:\Users\Public\Desktop\Driver Booster 8.lnk"
 Stop-Process -Name Driver*
 Write-Host "Done"
 
 Write-Host "Installing Movavi Video Editor Plus"
 # Not listed yet
 choco install movavivideoeditorplus --version=21.1.0
-del "C:\Users\$CurrentUserName\Desktop\Movavi Video Editor Plus 2021.lnk"
+Remove-Item "C:\Users\$CurrentUserName\Desktop\Movavi Video Editor Plus 2021.lnk"
 Write-Host "Done"
 
 Write-Host "Installing Film Info! Organiser"
 # Not listed yet
 choco install filminfo --version=0.6.1.3
-del "C:\Users\Public\Desktop\Film Info! Organizer.lnk"
+Remove-Item "C:\Users\Public\Desktop\Film Info! Organizer.lnk"
 New-Item -Path "C:\Users\$CurrentUserName\AppData\Roaming\film info! organizer" -ItemType directory -Force | Out-Null
 Start-BitsTransfer -Source "https://raw.githubusercontent.com/RalfEs73/chocolatey-packages/master/filminfo/settings/Columns.xml" -Destination "C:\Users\$CurrentUserName\AppData\Roaming\film info! organizer\Columns.xml"
 Start-BitsTransfer -Source "https://raw.githubusercontent.com/RalfEs73/chocolatey-packages/master/filminfo/settings/Config.xml" -Destination "C:\Users\$CurrentUserName\AppData\Roaming\film info! organizer\Config.xml"
@@ -189,5 +188,6 @@ foreach ($Log in $Logfiles) {
         Remove-Item $Log
         }
     }
+Remove-Item C:\Temp -Recurse
 
 New-BurntToastNotification -Text "Installation abgeschlossen", 'Die Installationen der Anwendungen wurden erfolgreich abgeschlossen'

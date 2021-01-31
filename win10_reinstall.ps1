6ff704a6bc3230546d626d4d040264e4610c4eff
RalfEs73 <ralf@essbaumer.de>
2021-01-31 22:06:45 +0100

    Update win10_reinstall.ps1

--- win10_reinstall.ps1
+++ win10_reinstall.ps1
@@ -8,6 +8,7 @@ $CurrentUserName = $CurrentUser.split("\")[1]
 
 Install-PackageProvider -Name NuGet -Force
 Install-Module -Name BurntToast -Force
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
 
 Write-Host "Installing Chocolatey"
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))

ec638c3d87a52c72f395763a33f579399f1b3a07
RalfEs73 <ralf@essbaumer.de>
2021-02-12 15:39:54 +0100

    Update win10_reinstall.ps1

--- win10_reinstall.ps1
+++ win10_reinstall.ps1
@@ -14,6 +14,7 @@ Write-Host "Installing Chocolatey"
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install chocolatey-core.extension -y $cache
 choco feature enable --name=allowGlobalConfirmation
+choco feature enable -n=useRememberedArgumentsForUpgrades
 Write-Host "Done"
 
 Write-Host "Installing Chocolatey GUI"
